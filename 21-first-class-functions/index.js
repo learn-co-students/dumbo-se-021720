a14b0b2b8fb12510ef7e5cf319815b9f99eab978
Ian Hollander <ianholla@gmail.com>
2020-03-30 15:45:57 -0400

    before first class fns

---
+++ 21-first-class-functions/index.js
@@ -0,0 +1,16 @@
+// /******* Hoisting *******/
+
+// /******* var, let, const, (global) *******/
+
+// /******* Lexical Scoping *******/
+
+// /******* global vs function vs block scope *******/
+
+// /******* scope in nested functions *******/
+
+// /******* 1st Class Functions *******/
+// 1. functions can be assigned to a variable
+
+// 2. functions can be passed around as arguments to other functions
+
+// 3. functions can be returned from functions
